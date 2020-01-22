3d91913bba69bad338fa287e2615dc845c07cc51
ern0ld <epa.suvila@gmail.com>
2020-01-22 16:48:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function lightsensor(){
+    if ( 'AmbientLightSensor' in window ) {
+      const sensor = new AmbientLightSensor();
+      sensor.onreading = () => {
+        console.log('Current light level:', sensor.illuminance);
+        if(sensor.illuminance > 100) {
+          alert("Onpa kirkasta! Nkykyinen taso: " + sensor.illuminance);
+        }
+      };
+      sensor.onerror = (event) => {
+        console.log(event.error.name, event.error.message);
+      };
+      sensor.start();
+    }
+    else {
+      console.log("Valosensori ei käytettävissä");
+    }
+    
+  }
